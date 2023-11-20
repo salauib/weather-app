ef2319bca66a124ee92a4e9da421816d9e1aa867
Salau Ibrahim <officialsalau@gmail.com>
2023-11-20 22:35:43 +0100

    Another commit

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 const apikey = "aecfb34476c16098fddea4d1048d9c65";
+
+const weatherDataElement = document.getElementById("weather-data");
+
+const cityInputElement = document.getElementById("city-input");
+
