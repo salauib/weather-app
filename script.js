2fad716b3b6772e7eaa53c2f6cf11557c3e0dbed
Salau Ibrahim <officialsalau@gmail.com>
2023-11-23 18:07:03 +0100

    Another commit to JavaScript file

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ formEl.addEventListener("submit", (event)=> {
     getWeatherData(cityValue);
 });
 
-function getWeatherData(cityValue){
+async function getWeatherData(cityValue){
     try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apikey}&units=metric`)
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apikey}&units=metric`);
     
         if(!response.ok){
             throw new Error("Network response was not ok");
@@ -23,7 +23,21 @@ function getWeatherData(cityValue){
 
         const data = await response.json();
 
-        console.log(data);
+        const temperature = Math.round(data.main.temp);
+
+        const description = data.weather[0].description;
+
+        const icon = data.weather[0].icon;
+
+        const details = [
+            `Feels like: ${Math.round(data.main.feels_like)}`,
+            `Humidity: ${data.main.humidity}`,
+            `Wind speed: ${data.wind.speed}`
+        ];
+
+        weatherDataEl.querySelector(".icon").innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}.png" alt="Weather Icon">`;
+    
+        weatherDataEl.querySelector(".temperature").textContent = `${temperature}\xB0`;
     } catch (error) {
         
     }
